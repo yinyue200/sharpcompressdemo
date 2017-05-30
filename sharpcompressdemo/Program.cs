9f144b0724a505f4d4a41b040b2e3578cca6ec98
yinyue200 <yinyue200@outlook.com>
2017-05-30 21:44:50 +0800

    init

--- sharpcompressdemo/Program.cs
+++ sharpcompressdemo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace sharpcompressdemo
 {
@@ -10,6 +11,13 @@ namespace sharpcompressdemo
     {
         static void Main(string[] args)
         {
+            using (var file = System.IO.File.Create("D:\\a.zip"))
+            {
+                using (var zw = new SharpCompress.Writers.Zip.ZipWriter(file, new SharpCompress.Writers.Zip.ZipWriterOptions(SharpCompress.Common.CompressionType.Deflate)))
+                {
+                    zw.WriteToStream("a",new SharpCompress.Writers.Zip.ZipWriterEntryOptions()).Dispose();
+                }
+            }
         }
     }
 }
